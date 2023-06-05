3e530b101ca0ca60b1fc3440214da7d818c44651
Romain <maillyromain@gmail.com>
2023-06-05 15:42:16 +0200

    Premier commit

---
+++ variables.py
@@ -0,0 +1,53 @@
+#
+
+number1 = 123
+print(number1)
+
+number2 = 3.14
+print(number2)  
+
+
+text1 = jsp 
+
+print(text1)
+
+
+text2 = pp  
+
+print(text2)
+
+
+
+
+python_is_cool = True
+python_is_boring = False
+
+
+
+user_accepted_terms = None
+
+
+
+print(type(number1))
+print(type(number2))
+print(type(text1))
+print(type(text2))
+print(type(python_is_cool))
+print(type(python_is_boring))
+print(type(user_accepted_terms ))
+
+# verification
+
+print(type(number1 is int))
+print(type(number1 is str))
+
+
+
+
+# Todo: interroger le type de donnée
+ 
+text3 = '123456789'
+print(type(int : text3))
+
+
+
